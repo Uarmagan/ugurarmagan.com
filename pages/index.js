e61fdeeb57591c3eb0894509915a2a406430e603
Ugur Armagan <ugurarmagan93@gmail.com>
2021-01-18 15:35:44 -0600

    increasing the width of the page

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Container from '../components/container';
 export default function Home() {
   return (
     <Container>
-      <div className="flex flex-col items-center w-9/12 max-w-3xl mx-auto mt-20 space-y-5">
+      <div className="flex flex-col items-center w-11/12 max-w-3xl mx-auto mt-20 space-y-5">
         <Image
           src="/images/avatar.jpg"
           alt="Picture of the author"
@@ -13,11 +13,11 @@ export default function Home() {
           className="rounded-full"
         />
         <div className="flex flex-col items-center justify-center">
-          <h1 className="text-5xl font-bold tracking-wide text-center mb-3 leading-14">
+          <h1 className="mb-3 text-4xl font-bold tracking-wide text-center leading-14">
             HI, MY NAME IS UGUR
           </h1>
-          <h3 className="text-xl my-2">Welcome To My Internet Home</h3>
-          <p className="my-8 text-gray-400 prose-lg items-center w-11/12">
+          <h3 className="my-2 text-xl">Welcome To My Internet Home</h3>
+          <p className="items-center w-11/12 my-8 text-gray-400 prose-lg">
             I’m a Full-Stack Developer based in Chicago with a focus in
             Javascript and Typescript. I write about my learnings on my Blog. If
             you have any web development needs, please reach out! If you want to
