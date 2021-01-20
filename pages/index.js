a1635a23fdf504e70475fd6a702065d5f27f7e88
Ugur Armagan <ugurarmagan93@gmail.com>
2021-01-19 20:39:40 -0600

    text centered on mobile

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
             <h3 className="text-xl sm:text-2xl">Welcome To My Internet Home</h3>
           </div>
         </div>
-        <p className="items-center w-11/12 mt-8 prose-lg">
+        <p className="items-center w-10/12 mt-8 text-center sm:text-left prose-lg">
           I’m a Full-Stack Developer based in Chicago with a focus in Javascript
           and Typescript. I write on life, building on the internet, and tech.
           If you have any web development needs, please reach out! If you want
