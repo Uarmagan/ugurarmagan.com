473d67d2af2caf1a53b95e1587fc78d5f403aba2
Ugur Armagan <ugurarmagan93@gmail.com>
2021-02-17 20:26:19 -0600

    adds preliminary first blog post

--- components/blog.js
+++ components/blog.js
@@ -5,12 +5,12 @@ import Container from './container';
 export default function BlogLayout({ children, frontMatter }) {
   return (
     <Container>
-      <div className="py-32 overflow-hidden text-white">
-        <div className="px-4 sm:px-6 lg:px-8">
+      <div className="py-32 overflow-hidden">
+        <article className="px-4 sm:px-6 lg:px-8">
           <Link href="/">
             <a className="text-sm text-gray-400">← Go Back</a>
           </Link>
-          <div className="flex flex-col jusitfy-left space-y-3">
+          <div className="flex flex-col justify-left space-y-3">
             <h1 className="block text-3xl font-extrabold tracking-tight text-orange sm:text-4xl">
               {frontMatter.title}
             </h1>
@@ -18,10 +18,10 @@ export default function BlogLayout({ children, frontMatter }) {
               {format(parseISO(frontMatter.publishedAt), 'MMMM dd, yyyy')}
             </p>
           </div>
-          <article className="mx-auto mt-5 prose-dark prose-lg leading-8">
+          <div className="mx-auto mt-5 prose dark:prose-dark prose-lg leading-8">
             {children}
-          </article>
-        </div>
+          </div>
+        </article>
       </div>
     </Container>
   );
--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home({ posts }) {
             <h3 className="text-xl sm:text-2xl">Welcome To My Internet Home</h3>
           </div>
         </div>
-        <p className="items-center mt-8 text-center text-gray-300 sm:w-11/12 sm:text-left md:prose-lg">
+        <p className="items-center mt-8 text-center text-gray-300 sm:w-11/12 sm:text-left prose prose-lg">
           I’m a Full-Stack Developer based in Chicago with a focus in Javascript
           and Typescript. I write a little about life, building on the internet,
           and tech. If you have any web development needs, please reach out! If
