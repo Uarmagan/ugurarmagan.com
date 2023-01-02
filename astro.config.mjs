e96bbc6fe2ad3c60578241ed410044cf2fbdd052
Ugur Armagan <ugurarmagan93@gmail.com>
2023-01-02 15:42:16 -0800

    vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,11 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), tailwind()],
+  integrations: [mdx(), tailwind()],
+  output: "server",
+  adapter: vercel()
 });
