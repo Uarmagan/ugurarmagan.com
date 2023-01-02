06b55ce6168f7f523742555e246f9511869fb692
Ugur Armagan <ugurarmagan93@gmail.com>
2023-01-02 15:37:53 -0800

    Initial commit

---
+++ src/scripts/fadeIn.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const title = document.querySelector('#title');
+  const body = document.querySelector('#body');
+  const links = document.querySelector('#links');
+  title.classList.add('opacity-100');
+  body.classList.add('opacity-100');
+  links.classList.add('opacity-100');
+});
