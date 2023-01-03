034d99008c151a81dc6803127f2811bb2360608c
Ugur Armagan <ugurarmagan93@gmail.com>
2023-01-02 16:03:57 -0800

    typos and cleaup

--- src/scripts/fadeIn.js
+++ src/scripts/fadeIn.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const title = document.querySelector('#title');
   const body = document.querySelector('#body');
+  const resume = document.querySelector('#resume');
   const links = document.querySelector('#links');
   title.classList.add('opacity-100');
   body.classList.add('opacity-100');
+  resume.classList.add('opacity-100');
   links.classList.add('opacity-100');
 });
